4b1d6a3dcd19e4018b4596189a8b53f17e72ab89
adal877 <adalberto.luiz.santos.junior@gmail.com>
2022-06-19 19:28:52 -0300

    Updated files

--- src/main/java/org/example/OpencvTest.java
+++ src/main/java/org/example/OpencvTest.java
@@ -1,12 +1,36 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.example.view.DisplayWindow;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.IOException;
+
 public class OpencvTest {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static String rootPath = "/Users/adal/git-sources/opencv/samples/winrt/FaceDetection/FaceDetection/Assets/";
+    public static int FaceDetection(String imgFile) {
+        // Read the image from the file storing it in to a matrix object
+        Mat srcImg = Imgcodecs.imread(imgFile);
+        // Make an instance of the CascadeClassifier
+        String xmlFace = rootPath + "haarcascade_frontalface_alt.xml";
+        CascadeClassifier classifier = new CascadeClassifier(xmlFace);
+        // Detect the face(s)
+        MatOfRect facesDetected = new MatOfRect();
+        classifier.detectMultiScale(srcImg, facesDetected);
+        System.out.println("::::: Faces :::::");
+        System.out.println(facesDetected);
+        System.out.println(":::::::::::::::::");
+        return facesDetected.toArray().length;
+    }
+    public static void main( String[] args ) throws IOException {
+        nu.pattern.OpenCV.loadLocally();
+        String imgFile = rootPath + "group1.jpg";
+        int qtdFaces = FaceDetection(imgFile);
+        System.out.printf("Detected %s face(s)!", qtdFaces);
+        String frameText = "Detected " + String.valueOf(qtdFaces) + " faces!";
+
+        DisplayWindow.getInstance(frameText).DisplayImg(imgFile);
     }
 }
--- src/main/java/org/example/view/DisplayWindow.java
+++ src/main/java/org/example/view/DisplayWindow.java
@@ -18,38 +18,63 @@ public class DisplayWindow extends JFrame implements KeyListener {
     private BufferedImage img;
     private int imgWidth;
     private int imgHeight;
+    private Boolean isDefaultValue = false;
+    private String OnFrameText = "";
+    private int positionX = 0;
+    private int positionY = 0;
     /*
      * Makes an instance of it self.
      * Cut off the need to make an object instance
      * just to use the constructor.
      */
+    public static DisplayWindow getInstance(String text) {
+        return new DisplayWindow(text);
+    }
     public static DisplayWindow getInstance() {
         return new DisplayWindow();
     }
-    public static DisplayWindow getInstance(int width, int height) {
-        return new DisplayWindow(width, height);
+    public static DisplayWindow getInstance(int width, int height, String text) {
+        return new DisplayWindow(width, height, text);
     }
-    public DisplayWindow(int width, int height) {
-        imgHeight = height;
-        imgWidth  = width;
-        drawWindow(width, height);
+    public DisplayWindow(int width, int height, String text) {
+        setImgWidth(width);
+        setImgHeight(height);
+        setOnFrameText(text);
+        drawWindow(getImgWidth(), getImgHeight());
     }
+
     public DisplayWindow() {
-        drawWindow();
+        setIsDefaultValue(true);
+        setImgWidth(0);
+        setImgHeight(0);
+        drawWindow(getImgWidth(), getImgHeight());
     }
-    private void drawWindow() {
-        frame = new JFrame();
-        frame.setLayout(new FlowLayout());
-        frame.setSize(600, 200);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public DisplayWindow(String text) {
+        setIsDefaultValue(true);
+        setImgWidth(0);
+        setImgHeight(0);
+        setOnFrameText(text);
+        drawWindow(getImgWidth(), getImgHeight());
     }
     /*
      * Creates the default window.
      */
     private void drawWindow(int width, int height) {
+        setPositionX(2);
+        setPositionY(2);
         frame = new JFrame();
+        lbl = new JLabel() {
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Color c = new Color(0xFFFFFF);
+                g.setColor(c);
+                g.fillRect(getPositionX(), getPositionY(), getOnFrameText().length()*8, 20);
+                Color c2 = new Color(0);
+                g.setColor(c2);
+                g.drawString(getOnFrameText(), getPositionX()+8, getPositionY()+14); //these are x and y positions
+                g.drawRect(getPositionX()-1, getPositionY()-1, (getOnFrameText().length()*8)+1, 21);
+            }
+        };
         frame.setLayout(new FlowLayout());
         frame.setSize(width, height);
         frame.setLocationRelativeTo(null);
@@ -60,11 +85,16 @@ public class DisplayWindow extends JFrame implements KeyListener {
         this.imgPath = imgPath;
         img = ImageIO.read(new File(imgPath));
         icon = new ImageIcon(img);
-        imgHeight = img.getHeight();
-        imgWidth  = img.getWidth();
-        lbl = new JLabel();
+        /*
+         * Assumes the image dimensions as values for the window.
+         */
+        if(isDefaultValue) {
+            setImgHeight(img.getHeight());
+            setImgWidth(img.getWidth());
+        }
         lbl.setIcon(icon);
-        frame.setSize(imgWidth, imgHeight);
+        frame.setSize(getImgWidth(), getImgHeight());
+        frame.repaint();
         frame.add(lbl);
     }
     @Override
@@ -86,8 +116,41 @@ public class DisplayWindow extends JFrame implements KeyListener {
     public int getImgWidth() {
         return imgWidth;
     }
-
+    private void setImgWidth(int imgWidth) {
+        this.imgWidth = imgWidth;
+    }
     public int getImgHeight() {
         return imgHeight;
+    }
+    private void setImgHeight(int imgHeight) {
+        this.imgHeight = imgHeight;
+    }
+    private Boolean getIsDefaultValue() {
+        return isDefaultValue;
+    }
+    private void setOnFrameText(String OnFrameText) {
+        this.OnFrameText = OnFrameText;
+    }
+    private String getOnFrameText() {
+        return OnFrameText;
+    }
+    private void setIsDefaultValue(Boolean defaultValue) {
+        isDefaultValue = defaultValue;
+    }
+
+    private int getPositionX() {
+        return positionX;
+    }
+
+    private void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    private int getPositionY() {
+        return positionY;
+    }
+
+    private void setPositionY(int positionY) {
+        this.positionY = positionY;
     }
 }
